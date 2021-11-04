bc744c55a7ef7cd2efe935de6ef3ca1dad099564
ji-mmyliu <jimmyjcl753@gmail.com>
2021-11-04 12:48:58 -0400

    Retract removal of data.out file

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ def clearFile(filename):
     f.close()
 
 def clearSources(judgeNum):
-    clearFile("Judge" + str(judgeNum) + "/data.out")
+    #clearFile("Judge" + str(judgeNum) + "/data.out")
     clearFile("Judge" + str(judgeNum) + "/data.in")
     clearFile("Judge" + str(judgeNum) + "/expected.out")
 
