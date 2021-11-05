631e8159cca9cf8449dfabfdd16cf82034dfec39
ji-mmyliu <jimmyjcl753@gmail.com>
2021-11-05 12:06:39 -0400

    Sandboxing debug

--- sandboxing.py
+++ sandboxing.py
@@ -5,11 +5,10 @@ from RestrictedPython.PrintCollector import PrintCollector
 from RestrictedPython import compile_restricted
 from RestrictedPython.Guards import safe_builtins
 restricted_globals = dict(__builtins__=safe_builtins)
-
 _print_ = PrintCollector
 _getattr_ = getattr
 
 src = open(sys.argv[1]).read()
 
 code = compile_restricted(src, '<string>', 'exec')
-exec(code, restricted_globals)
+exec(code)
