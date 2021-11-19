3e958f346ec4d7fba44499c57bc6891511359cac
ji-mmyliu <jimmyjcl753@gmail.com>
2021-11-19 00:05:38 -0500

    Fix timelim variable bug

--- main.py
+++ main.py
@@ -317,7 +317,7 @@ async def on_message(message):
             rpc = Process(target = runSubmission, args = (judges, username, cleaned, lang, problm, attachments, filename, return_dict))
             rpc.start()
 
-            msg = "EXECUTION RESULTS\n" + username + "'s submission for " + problem + " in " + lang + "\n" + ("Time limit for this problem in " + lang + ": {x:.2f} seconds".format(x = timelim)) + "\nRunning on Judging Server #" + str(judgeNum) + "\n\n"
+            msg = "EXECUTION RESULTS\n" + username + "'s submission for " + problem + " in " + lang + "\n" + ("Time limit for this problem in " + lang + ": {x:.2f} seconds".format(x = problm['timelim'])) + "\nRunning on Judging Server #" + str(judgeNum) + "\n\n"
             curmsg = await message.channel.send("```" + msg + "(Status: WAITING FOR JUDGE RESPONSE)```")
 
             msgContent = "```" + msg + "(Status: WAITING FOR JUDGE RESPONSE)```"
