79c75192ea20c7fc99d603e14cf00ac22b0ef5d1
ji-mmyliu <jimmyjcl753@gmail.com>
2021-11-29 14:34:09 -0500

    Exception debugging

--- ProblemUpload.py
+++ ProblemUpload.py
@@ -29,25 +29,33 @@ def uploadProblem(settings, storage_client, url, author):
         pass
     settings.insert_one({"type":"problem", "name":params['name'], "points":params['difficulty'], "status":"s", "published":params['private'] == 0, "contest":contest})
     
-    batches = params['batches']
-    for x in range(1, len(batches) + 1):
-        for y in range(1, batches[x - 1] + 1):
-            upload_blob(storage_client, "problemdata/data" + str(x) + "." + str(y) + ".in", "TestData/" + params['name'])
-            upload_blob(storage_client, "problemdata/data" + str(x) + "." + str(y) + ".out", "TestData/" + params['name'])
+    try:
+        batches = params['batches']
+        for x in range(1, len(batches) + 1):
+            for y in range(1, batches[x - 1] + 1):
+                upload_blob(storage_client, "problemdata/data" + str(x) + "." + str(y) + ".in", "TestData/" + params['name'])
+                upload_blob(storage_client, "problemdata/data" + str(x) + "." + str(y) + ".out", "TestData/" + params['name'])
+    except Exception as e:
+        print(str(e))
+        return "Error with uploading testdata"
     
-    cases = open("problemdata/cases.txt", "w")
-    for x in batches:
-        cases.write(str(x) + " ")
-    cases.write("\n")
-    for x in params['points']:
-        cases.write(str(x) + " ")
-    cases.write("\n")
-    for x in params['timelimit']:
-        cases.write(str(x) + " ")
-    cases.write("\n")
-    cases.flush()
-    cases.close()
-    upload_blob(storage_client, "problemdata/cases.txt", "TestData/" + params['name'])
+    try:
+        cases = open("problemdata/cases.txt", "w")
+        for x in batches:
+            cases.write(str(x) + " ")
+        cases.write("\n")
+        for x in params['points']:
+            cases.write(str(x) + " ")
+        cases.write("\n")
+        for x in params['timelimit']:
+            cases.write(str(x) + " ")
+        cases.write("\n")
+        cases.flush()
+        cases.close()
+        upload_blob(storage_client, "problemdata/cases.txt", "TestData/" + params['name'])
+    except Exception as e:
+        print(str(e))
+        return "Error with uploading cases"
     
     if not existingProblem is None:
         if author != existingProblem['author']:
